e488a04032e2fc7ca65c3355731666a3a8ab6c37
Carlol6969 <142802832+Carlol6969@users.noreply.github.com>
2024-04-09 11:12:03 -0300

    navegação

--- src/pages/componenetes/menu/index.js
+++ src/pages/componenetes/menu/index.js
@@ -1,8 +1,13 @@
+import {Link} from 'react-router-dom'
 export default function menu(){
     return(
         <div>
             <nav>
                 <h1> Menu</h1>
+                <Link to="" className='link'>
+                    Usuário
+
+                </Link>
             </nav>
         </div>
 
